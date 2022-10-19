75aa1dd3702248e9027f5e852f4f46d4ef276745
Amy Sandoval <amysandoval92@gmail.com>
2022-10-18 17:22:55 -0700

    Add structure to Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,7 +2,15 @@ import React from "react";
 import styles from "../Footer/Footer.module.css";
 
 function Footer() {
-  return <div className={styles.container}>This will be the Footer!</div>;
+  return (
+    <div className={styles.container}>
+      <ul className={styles.links}>
+        <li>Instagram Link</li>
+        <li>Facebook Link</li>
+        <li>Email Link</li>
+      </ul>
+    </div>
+  );
 }
 
 export default Footer;
