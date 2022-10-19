3e78f8124b1be39fb706a5e8f0ab9a7e44d8b025
Amy Sandoval <amysandoval92@gmail.com>
2022-10-18 22:03:32 -0700

    Updated Gallery flex display to grid display

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -53,6 +53,21 @@ function Gallery() {
             <button className={styles.button}>Order Online</button>
           </div>
         </div>
+        <div className={styles.card}>
+          <div className={styles.content}>
+            <img
+              className={styles.image}
+              src="./images/girl-with-red-hat-UnSvjoojVO4-unsplash.jpg"
+              alt="Cookies"
+            ></img>
+            <h2 className={styles.title}>Cookies</h2>
+            <p className={styles.body}>
+              Vegan cookies. Gluten-free cookies. Even oil-free cookies. Your
+              choice.
+            </p>
+            <button className={styles.button}>Order Online</button>
+          </div>
+        </div>
       </div>
     </div>
   );
