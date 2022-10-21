d31687b424841cffd35318f9b092d17215c7ec4e
Amy Sandoval <amysandoval92@gmail.com>
2022-10-21 12:40:19 -0700

    Install react-router-dom and set up HashRouter in App.js

--- src/App.js
+++ src/App.js
@@ -2,15 +2,26 @@ import Navigation from "./components/Navigation/Navigation";
 import Headline from "./components/Headline/Headline";
 import Gallery from "./components/Gallery/Gallery";
 import Footer from "./components/Footer/Footer";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <Navigation />
-      <Headline />
-      <Gallery />
-      <Footer />
-    </div>
+    <HashRouter>
+      <Routes>
+        <Route
+          exact
+          path="/"
+          element={
+            <div>
+              <Navigation />
+              <Headline />
+              <Gallery />
+              <Footer />
+            </div>
+          }
+        />
+      </Routes>
+    </HashRouter>
   );
 }
 
